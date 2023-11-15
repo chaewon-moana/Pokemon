2a100edbf6ded86c32f4ac5709616194556211d8
cho <chaewon@chos-MacBook-Pro.local>
2023-11-15 23:16:41 +0900

    first commit

---
+++ PockmonAPI/PockmonAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PockmonAPIApp.swift
+//  PockmonAPI
+//
+//  Created by cho on 2023/11/15.
+//
+
+import SwiftUI
+
+@main
+struct PockmonAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PockmonAPI/Pokemon.swift
@@ -0,0 +1,34 @@
+//
+//  Pokemon.swift
+//  PockmonAPI
+//
+//  Created by cho on 2023/11/15.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+    let name: String
+    let sprites: Sprites
+    let types: [Types]
+    let weight: Int
+    let height: Int
+}
+
+struct PokemonType: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+}
+
+
+struct Sprites: Codable {
+    let front_default: String?
+}
+
+struct Types: Codable {
+    let type: TypeInfo
+}
+
+struct TypeInfo: Codable {
+    let name: String
+}
